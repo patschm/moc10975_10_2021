10fc32faa6c24cf74b239e37eccf176d4ce97200
Patrick Schmidt <pschmidt@pasit.nl>
2021-10-20 13:35:36 +0200

    second

---
+++ SorterenSolution/Fouten/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fouten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+          
+
+            try
+            {  
+                int y = int.Parse(Console.ReadLine());
+                double result =  Divide(x, y);
+                Console.WriteLine(result);
+            }
+            catch(OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+                Console.WriteLine("Getal de root of te klein");
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Ongeldig getal");
+            }
+            catch(DivideByZeroException de)
+            {
+                Console.WriteLine("Delen door 0 is flauwekul");
+                return;
+            }
+           catch(Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Das nie goe");
+            }
+            finally
+            {
+                Console.WriteLine("Tenslotte...");
+            }
+            Console.ReadLine();
+        }
+
+        static double Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ SorterenSolution/Sorteren/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Sorteren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorteren();
+        }
+
+        static void Sorteren()
+        {
+            int[] data = new int[6] { 3, 7, 4, 9, 2, 5 };
+            ShowData(data);
+
+
+            bool isSwapped;
+            do
+            {
+                isSwapped = false;
+                for (int i = 0; i < data.Length - 1; i++)
+                {
+                    if (data[i] > data[i + 1])
+                    {
+                        int tmp = data[i];
+                        data[i] = data[i + 1];
+                        data[i + 1] = tmp;
+                        isSwapped = true;
+                    }
+                }
+            }
+            while (isSwapped);
+            ShowData(data);
+        }
+
+        static void ShowData(int[] data)
+        {
+            for(int idx = 0; idx < data.Length; idx++)
+            {
+                int nr = data[idx];
+                Console.Write(nr);
+                if (idx < data.Length - 1)
+                {
+                    // Laatse element hoeft geen komma achter
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
